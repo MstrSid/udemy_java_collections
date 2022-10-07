76a228c6281f13c6ad18311bbc3295b656293846
Eugene Kos <mstrsid@live.com>
2022-10-07 15:29:02 +0300

    done

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Car {
     private String brand;
     private int number;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,10 @@
 package org.example;
 
-import org.example.customarraylist.CarListArray;
-
 public class Main {
     public static void main(String[] args) {
-        CarListArray carList = new CarListArray();
-        for (int i = 0; i < 100; i++) {
-            carList.add(new Car("Brand" + i, i));
-        }
-        System.out.println(carList.size());
+        Car car1 = new Car("BMW", 1);
+        Car car2 = new Car("BMW", 1);
 
-        carList.removeAt(5);
-        System.out.println(carList.size());
-        carList.remove(new Car("Brand10", 10));
-        System.out.println(carList.size());
-        System.out.println(carList.get(20));
-        System.out.println(carList.size());
-
-
-        carList.add(new Car("BMW", 245), 1);
-        for (int i = 0; i < carList.size(); i++) {
-            System.out.println(carList.get(i));
-        }
-        System.out.println(carList.size());
+        System.out.println(car1.equals(car2));
     }
 }
